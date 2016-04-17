d88a1e84bc24017b6c4bab5cac277d4bda72110b
kschmidt2 <k.schmidt2@gmail.com>
2016-04-17 00:26:51 -0400

    adds rice and hardy pages and timeline plugin

---
+++ js/fullPage_options.js
@@ -0,0 +1,80 @@
+$(document).ready(function() {
+  $('#fullpage').fullpage({
+
+    //Navigation
+    // menu: '#menu',
+    // lockAnchors: false,
+    // anchors: ['firstPage', 'secondPage'],
+    // navigation: true,
+    // navigationPosition: 'right',
+    // navigationTooltips: ['First page', 'Second page', 'Third page', 'Fourth page', 'Fifth page'],      //have to figure out simplest way to customize this per page
+    // showActiveTooltip: false,
+    // slidesNavigation: true,
+    // slidesNavPosition: 'bottom',
+    // //
+    // // //Scrolling
+    // css3: true,
+    // scrollingSpeed: 700,
+    autoScrolling: false,
+    fitToSection: false,
+    // fitToSectionDelay: 1000,
+    // scrollBar: false,
+    // easing: 'easeInOutCubic',
+    // easingcss3: 'ease',
+    // loopBottom: false,
+    // loopTop: false,
+    // loopHorizontal: true,
+    // continuousVertical: false,
+    // normalScrollElements: '#element1, .element2',
+    // scrollOverflow: true,
+    // touchSensitivity: 75,
+    // normalScrollElementTouchThreshold: 5,
+    // //
+    // // //Accessibility
+    // keyboardScrolling: true,
+    // animateAnchor: true,
+    // recordHistory: false,
+    // responsiveWidth: 768,
+    // //
+    // // //Design
+    // sectionsColor: ['#FFFFFF'],
+    // controlArrows: false,
+    // verticalCentered: true,
+    // resize : false,
+    // paddingTop: '1.5em',
+    // paddingBottom: '3.5em',
+    // fixedElements: '#header, #logo, #switch_language',
+    responsiveWidth: 1100,
+    // responsiveHeight: 0,
+
+    //events
+    afterLoad: function(anchorLink, index){
+      if(index == 2){
+                console.log("Section 2 loaded");
+                $('#rice-sidebar').fadeIn(100);
+                $('#rice-sidebar').animate({left: "0"}, 500);
+                $('#hardy-sidebar').fadeIn(100);
+                $('#hardy-sidebar').animate({right: "0"}, 500);
+                $('#logo').delay(500).fadeIn(100);
+                $('.view-timeline-mobile').delay(500).fadeIn(100);
+            }
+
+    },
+    onLeave: function(index, nextIndex, direction){
+            var leavingSection = $(this);
+            console.log("onLeave()")
+
+            //after leaving section 5
+            if(index == 1 && direction == 'up'){
+                console.log("Leaving section 2!");
+                $('#rice-sidebar').hide();
+                $('#rice-sidebar').animate({left: "-18%"}, 500);
+                $('#rice-sidebar').fadeOut(100);
+                $('#hardy-sidebar').animate({right: "-18%"}, 500);
+                $('#hardy-sidebar').fadeOut(100);
+                $('#logo').fadeOut(100);
+                $('.view-timeline-mobile').fadeOut(100);
+            }
+        },
+  });
+});
--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,10 @@
 
-$(document).ready(function() {
-	$('#fullpage').fullpage({
-		autoScrolling: false,
-		fitToSection: false,
-    responsiveWidth: 1100
-	});
+// $(document).ready(function() {
+// 	$('#fullpage').fullpage({
+// 		autoScrolling: false,
+// 		fitToSection: false,
+//     responsiveWidth: 1100
+// 	});
 
 	// $('#main').scroll(function(){
 	// 	$('#rice-sidebar').animate({left: "0"}, 500);
@@ -15,29 +15,29 @@ $(document).ready(function() {
 	// 	$('#hardy-sidebar').animate({right: "-18%"}, 100);
 	// });
 
-	var targetOffset = $("#main").offset().top;
-
-	var $w = $(window).scroll(function(){
-	    if ( $w.scrollTop() > targetOffset ) {
-				console.log("blergh");
-				$('#rice-sidebar').fadeIn(100);
-				$('#rice-sidebar').animate({left: "0"}, 500);
-				$('#hardy-sidebar').fadeIn(100);
-				$('#hardy-sidebar').animate({right: "0"}, 500);
-				$('#logo').show(100);
-				$('.view-timeline-mobile').show(100);
-			}
-			var $h = $(window).scroll(function(){
-			    if ( $h.scrollTop() == 0 ) {
-						console.log("blahhh");
-						$('#rice-sidebar').stop().css({left:"-18%"});
-						$('#hardy-sidebar').stop().css({right: "-18%"});
-						$('#logo').hide(100);
-						$('.view-timeline-mobile').hide(100);
-					}
-				});
-
-	});
+	// var targetOffset = $("#main").offset().top;
+	//
+	// var $w = $(window).scroll(function(){
+	//     if ( $w.scrollTop() > targetOffset ) {
+	// 			console.log("blergh");
+	// 			$('#rice-sidebar').fadeIn(100);
+	// 			$('#rice-sidebar').animate({left: "0"}, 500);
+	// 			$('#hardy-sidebar').fadeIn(100);
+	// 			$('#hardy-sidebar').animate({right: "0"}, 500);
+	// 			$('#logo').show(100);
+	// 			$('.view-timeline-mobile').show(100);
+	// 		}
+	// 		var $h = $(window).scroll(function(){
+	// 		    if ( $h.scrollTop() == 0 ) {
+	// 					console.log("blahhh");
+	// 					$('#rice-sidebar').stop().css({left:"-18%"});
+	// 					$('#hardy-sidebar').stop().css({right: "-18%"});
+	// 					$('#logo').hide(100);
+	// 					$('.view-timeline-mobile').hide(100);
+	// 				}
+	// 			});
+	//
+	// });
 
 	// var homeOffset = $("#home").offset().top;
 	//
@@ -48,7 +48,7 @@ $(document).ready(function() {
 	//     }
 	// });
 
-});
+// });
 
 $('#logo').affix({
     // offset: {
